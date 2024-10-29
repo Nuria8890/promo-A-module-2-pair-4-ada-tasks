a82ec6d357787046a3962959654084387dd91dd1
Nuria Campo <nuria.campo7@gmail.com>
2024-10-29 11:23:02 +0100

    empezando a crear el listado de tareas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 'use strict';
-// La página web carga mejor, es más obtima
-// Resalta más los errores
-// 
+
+const ul = document.querySelector('.js-ul');
+
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
+
+
+/* Pintar en la pantalla todas las tareas que hay en el array tasks
+
+- seleccionar la ul del html
+
+*/
