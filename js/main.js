b3a4676da2c9caf729d6b2a83eda710a7cf6b4fc
Nuria Campo <nuria.campo7@gmail.com>
2024-10-30 16:53:49 +0100

    ejercicio 1 Vamos a darle dinamismo

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const tasks = [
 for (const task of tasks) {
   ul.innerHTML += `
   <li class="${task.completed === true ? 'strike' : ""}">
-    <input type="checkbox" id="${task.id}" name="tasks" value="${task.id}">
+    <input type="checkbox" id="${task.id}" name="tasks" value="${task.id}" ${task.completed === true ? 'checked' : ""}>
     <label for="${task.id}">
     ${task.name}
     </label>
@@ -27,13 +27,41 @@ for (const task of tasks) {
 }
 
 const handleClick = (event) => {
+  console.log('event.currentTarget es:', event.currentTarget)
+  console.log('event.target es:', event.target)
   const taskId = parseInt(event.target.id);
   console.log(taskId);
   if (!taskId) return;
 
-//   tasks.find((taskId) => {
-//     console.log('taskId es:', taskId);
-//   })
+  // Busca la tarea que tenga el id `taskId` en el array `tasks`
+  const checkedTask = tasks.find((task) => {
+    return task.id === taskId;
+  })
+  console.log('checkedTask es:', checkedTask);
+
+  // Una vez que has obtenido la tarea, actualiza la propiedad `completed`
+  checkedTask.completed = !checkedTask.completed;
+  
+  /* Lo de arriba es igual que esto: 
+    if (checkedTask.completed === true) {
+      checkedTask.completed = false;
+    } else {
+      checkedTask.completed = true;
+    }
+  */
+
+  // Pinta de nuevo las tareas en el html
+  ul.innerHTML = "";
+  for (const task of tasks) {
+    ul.innerHTML += `
+    <li class="${task.completed === true ? 'strike' : ""}">
+      <input type="checkbox" id="${task.id}" name="tasks" value="${task.id}" ${task.completed === true ? 'checked' : ""}>
+      <label for="${task.id}">
+      ${task.name}
+      </label>
+    </li>
+    `
+  }
 }
 
 ul.addEventListener('click', handleClick);
