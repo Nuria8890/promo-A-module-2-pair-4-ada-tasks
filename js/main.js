86ddf95c9f65175d9fdae3d42390a4a07bc1f140
Kristhinne <krs.opie@gmail.com>
2024-10-31 11:36:14 +0100

    tasks

--- js/main.js
+++ js/main.js
@@ -1,30 +1,42 @@
 'use strict';
 
+const GITHUB_USER = "krsalvarez";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 const ul = document.querySelector('.js-ul');
 
-const tasks = [
-  { name: "Recoger setas en el campo", completed: true, id: 1 },
-  { name: "Comprar pilas", completed: true, id: 2 },
-  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
-  {
-    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-    completed: false,
-    id: 4,
-  },
-];
+let tasks = [];
+
+ 
+ //[
+//   { name: "Recoger setas en el campo", completed: true, id: 1 },
+//   { name: "Comprar pilas", completed: true, id: 2 },
+//   { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+//   {
+//     name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+//     completed: false,
+//     id: 4,
+//   },
+// ];
+
+fetch(SERVER_URL).then((response) => response.json())
+.then((data) => {
+  tasks.push(data.results);
+  console.log(tasks);
+  }
+)
 
 // Pintar la lista en html
 
-for (const task of tasks) {
-  ul.innerHTML += `
-  <li class="${task.completed === true ? 'strike' : ""}">
-    <input type="checkbox" id="${task.id}" name="tasks" value="${task.id}">
-    <label for="${task.id}">
-    ${task.name}
-    </label>
-  </li>
-  `
-}
+//  for (const task of tasks) {
+//    ul.innerHTML += `
+//    <li class="${task.completed === true ? 'strike' : ""}">
+//      <input type="checkbox" id="${task.id}" name="tasks" value="${task.id}">
+//     <label for="${task.id}">
+//      ${task.name}
+//      </label>
+//   </li>
+//    `
+//  }
 
 const handleClick = (event) => {
   const taskId = parseInt(event.target.id);
