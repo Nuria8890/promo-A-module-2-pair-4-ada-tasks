6ee26a426ed46d8a643b54ec7452a912450e9930
Nuria Campo <nuria.campo7@gmail.com>
2024-11-02 18:25:06 +0100

    2.14 Ejercicio 1. Agregar una nueva tarea

--- js/main.js
+++ js/main.js
@@ -3,8 +3,27 @@
 const ul = document.querySelector(".js-ul");
 const GITHUB_USER = "Nuria8890";
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
+const inputAdd = document.querySelector(".js-inputAddTask");
+const btnAdd = document.querySelector(".js-btnAddTask");
 
 let tasks = [];
+
+// Pinta las tareas en el html
+const paintingTasks = () => {
+  ul.innerHTML = "";
+  for (const task of tasks) {
+    ul.innerHTML += `
+      <li class="${task.completed === true ? "strike" : ""}">
+        <input type="checkbox" id="${task.id}" name="tasks" value="${
+      task.id
+    }" ${task.completed === true ? "checked" : ""}>
+        <label for="${task.id}">
+        ${task.name}
+        </label>
+      </li>
+      `;
+  }
+};
 
 // Obtener el listado de tareas desde la API
 fetch(`https://dev.adalab.es/api/todo/${GITHUB_USER}`)
@@ -15,21 +34,10 @@ fetch(`https://dev.adalab.es/api/todo/${GITHUB_USER}`)
     tasks = data.results;
 
     // Pintar la lista en html
-    for (const task of tasks) {
-      ul.innerHTML += `
-      <li class="${task.completed === true ? "strike" : ""}">
-        <input type="checkbox" id="${task.id}" name="tasks" value="${
-        task.id
-      }" ${task.completed === true ? "checked" : ""}>
-        <label for="${task.id}">
-        ${task.name}
-        </label>
-      </li>
-      `;
-    }
+    paintingTasks();
   });
 
-const handleClick = (event) => {
+const handleClickList = (event) => {
   const taskId = parseInt(event.target.id);
   if (!taskId) return;
 
@@ -41,20 +49,24 @@ const handleClick = (event) => {
   // Una vez que has obtenido la tarea, actualiza la propiedad `completed`
   checkedTask.completed = !checkedTask.completed;
 
-  // Pinta de nuevo las tareas en el html
-  ul.innerHTML = "";
-  for (const task of tasks) {
-    ul.innerHTML += `
-    <li class="${task.completed === true ? "strike" : ""}">
-      <input type="checkbox" id="${task.id}" name="tasks" value="${task.id}" ${
-      task.completed === true ? "checked" : ""
-    }>
-      <label for="${task.id}">
-      ${task.name}
-      </label>
-    </li>
-    `;
-  }
+  // Pintar la lista en html
+  paintingTasks();
 };
 
-ul.addEventListener("click", handleClick);
+ul.addEventListener("click", handleClickList);
+
+const handleClickBtnAdd = (event) => {
+  event.preventDefault();
+  const inputTask = inputAdd.value;
+
+  const newTask = {
+    name: inputTask,
+    completed: false,
+  };
+  tasks.push(newTask);
+  console.log("tasks es", tasks);
+
+  paintingTasks();
+};
+
+btnAdd.addEventListener("click", handleClickBtnAdd);
